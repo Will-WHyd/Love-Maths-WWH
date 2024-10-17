5205ce54c3bfd290107643091be961b38abfa937
William Waldron-Hayden <williamjwh@gmail.com>
2024-10-17 17:21:53 +0000

    set up addition question

--- js/script.js
+++ js/script.js
@@ -10,14 +10,30 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame () {
 
+/**
+ * the main game "loop" called when the script is first loaded, 
+ * and after user's answer is processed
+ */
+function runGame (gameType) {
+    // creates 2 random numbers
+    let num1 = Math.ceil(Math.random()*25);
+    let num2 = Math.ceil(Math.random()*25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -36,7 +52,11 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 
 }
 
